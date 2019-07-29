c1f0d67243b9d4ddeec83f9c3030b406d74bfc51
Hu Yuanzhong <huyu@splunk.com>
2019-07-29 09:46:31 +0800

    format code

--- manage.py
+++ manage.py
@@ -9,4 +9,4 @@ app = create_app()
 
 # manage = Manager(app)
 if __name__ == '__main__':
-    app.run(host=app.config['HOST'],port=app.config['PORT'])
+    app.run(host=app.config['HOST'], port=app.config['PORT'])
